0703be238f807df5ece20fe5aa0e523e5b21eac7
codegenius8 <94888474+codegenius8@users.noreply.github.com>
2023-01-03 11:18:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public/uploads")));
 app.use("/", express.static("public"));
 
 
-//database connection connect
+
 // mongoose.Promise = global.Promise;
 // const PASSWORD = encodeURIComponent("");
 // const database = "AMA_project";
